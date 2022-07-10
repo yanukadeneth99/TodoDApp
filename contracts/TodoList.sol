a84880951a75ec08634f89832f2b87b39465c799
yanukadeneth99 <yanukadeneth99@gmail.com>
2022-07-10 22:24:56 +0530

    Final touches

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,6 +11,9 @@ contract TodoList {
 
   mapping(uint => Task) public tasks;
 
+  event TaskCreated(uint id, string content, bool completed);
+  event TaskCompleted(uint id, bool completed);
+
   constructor() public {
     createTask("Hello World!");
   }
@@ -18,5 +21,13 @@ contract TodoList {
   function createTask(string memory _content) public {
     taskCount++;
     tasks[taskCount] = Task(taskCount,_content,false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
   }
 }
